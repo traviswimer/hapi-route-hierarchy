fd6eb71229fae25c8951b9966e10591620941c94
Travis Wimer <notanactualemailaddress@traviswimer.com>
2017-11-19 21:56:48 -0600

    Updates for hapi@17.0.1 and node@8.9.1

--- index.js
+++ index.js
@@ -3,16 +3,12 @@
 let route_finder = require( './src/index' );
 
 const hapiRouteHierarchyPlugin = {
-	register: function( server, options, next ) {
+	pkg: require('./package.json'),
+	register: function( server, options ) {
 		route_finder( server, options, function( routes ) {
 			server.route( routes );
-			next();
 		} );
 	}
-};
-
-hapiRouteHierarchyPlugin.register.attributes = {
-	pkg: require( './package.json' )
 };
 
 module.exports = hapiRouteHierarchyPlugin;
